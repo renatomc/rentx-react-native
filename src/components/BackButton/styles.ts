de8f31707b566e6ff16d209b4b58ff4a70bdebe0
Renato Martinez Costa <renatomc@hotmail.com>
2023-08-02 18:50:28 -0300

    Car details images

--- src/components/BackButton/styles.ts
+++ src/components/BackButton/styles.ts
@@ -1,10 +1,4 @@
 import styled from 'styled-components/native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
-export const Container = styled(BorderlessButton)`
-  flex: 1;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  height: 100px;
-`;
+export const Container = styled(BorderlessButton)``;
--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-import { Container, Header } from './styles';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
+import { 
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from './styles';
 
 export function CarDetails() {
   return (
@@ -9,6 +24,34 @@ export function CarDetails() {
      <Header>
       <BackButton onPress={() => {}} />
      </Header>
+     <CarImages>
+      <ImageSlider imagesUrl={['https://pensecarros.com.br/cms/uploads/audi-rs5-2-9-v6-tfsi-gasolina-sportback-quattro-s-tronic-6130309e27d5e.png']} />
+     </CarImages>
+     <Content>
+      <Details>
+        <Description>
+          <Brand>
+            Lamborghini
+          </Brand>
+          <Name>
+            Huracan
+          </Name>
+        </Description>
+        <Rent>
+          <Period>
+            Ao dia
+          </Period>
+          <Price>
+            R$ 580
+          </Price>
+        </Rent>
+      </Details>
+      <About>
+        Este é automóvel desportivo. Surgiu do lendário 
+        touro de lide indultado na praça Real Maestranza de Sevilla.
+        É um belíssimo carro para quem gosta de acelerar.
+      </About>
+     </Content>
     </Container>
   );
 }
