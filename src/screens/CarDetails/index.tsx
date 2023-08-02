083c058f6e63180d259945b7e204bf323c46d5cd
Renato Martinez Costa <renatomc@hotmail.com>
2023-08-02 19:20:37 -0300

    Button component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
 
 import SpeedSvg from '../../assets/speed.svg';
 import AccelerationSvg from '../../assets/acceleration.svg';
@@ -25,6 +26,7 @@ import {
   Price,
   About,
   Acessories,
+  Footer,
 } from './styles';
 
 
@@ -70,6 +72,9 @@ export function CarDetails() {
         É um belíssimo carro para quem gosta de acelerar.
       </About>
      </Content>
+     <Footer>
+      <Button title="Confirmar" />
+     </Footer>
     </Container>
   );
 }
