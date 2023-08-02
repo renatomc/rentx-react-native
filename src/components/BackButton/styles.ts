b857ec328f7b2c3c0274e99ead6dd9d4af537b82
Renato Martinez Costa <renatomc@hotmail.com>
2023-08-02 17:03:28 -0300

    Button back

---
+++ src/components/BackButton/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+export const Container = styled(BorderlessButton)`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100px;
+`;
---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Header } from './styles';
+import { BackButton } from '../../components/BackButton';
+
+export function CarDetails() {
+  return (
+    <Container>
+     <Header>
+      <BackButton onPress={() => {}} />
+     </Header>
+    </Container>
+  );
+}
